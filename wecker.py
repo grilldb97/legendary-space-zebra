4e82f56b5456b4d5f2cf4d3d59f89e466caee0cf
grilldb97 <dustin.bellem@web.de>
2023-12-23 13:16:02 +0100

    23.12.2023 - Wecker: Buttons: Snooze/Stop getrennt; implementiert, dass man zw. Alarm und Musik wechseln kann; aktuell noch das Problem, dass der Alarm nicht funktioniert und die Tabs nicht blinken

--- wecker.py
+++ wecker.py
@@ -1,18 +1,35 @@
-# Teil 1
 import time
 from datetime import datetime
 from tkinter import *
-from tkinter import ttk
+from tkinter import filedialog
 import threading
 import winsound
 from queue import Queue
+from functools import partial
+import pygame
+import os
+from tkinter import ttk
+from tkinter import StringVar
+
+# Erstellen Sie die StringVar-Objekte hier, nachdem das Fenster erstellt wurde
+global button_labels
+
+# Erstellen Sie eine leere Liste von Tabs am Anfang Ihres Programms
+tabs = []
 
 snooze_time = 10  # Zeit in Minuten für die Snooze-Funktion
 alarm_on = [False, False, False]  # Zustand des Alarms für jede Weckzeit
 wecker_zeit = ["", "", ""]  # Speichert die Weckzeiten
 wecker_set = [False, False, False]  # Speichert, ob der Wecker eingestellt wurde
+alarm_typ = [0, 0, 0]  # Globale Liste der Alarmtypen (0 für winsound.beep, 1 für Musik)
+musik_dateien = ["nonexistent.mp3", "nonexistent.mp3", "nonexistent.mp3"]  # Globale Liste der Musikdateien
+
+
 # Erstellen Sie eine globale Warteschlange
 alarm_queue = Queue()
+
+# Globale Liste der Alarm-Threads
+alarm_threads = [threading.Thread(target=lambda: None) for _ in range(3)]
 
 
 def uhrzeit(time_label):
@@ -23,74 +40,82 @@ def uhrzeit(time_label):
         time.sleep(1)
 
 
-def alarm(index):
+def alarm_mit_beep(index):
     global alarm_on
     alarm_on[index] = True
     freq = 500
-    print(f"Alarm-Funktion für Wecker {index+1} wird gestartet")
-    # Fügt eine separate while-Schleife hinzu
     while alarm_on[index]:
-        # Die ursprüngliche while-Schleife wird zu einer inneren Schleife
-        for _ in range(10):  # Wiederholt den Alarm 10 Mal
-            if not alarm_on[index]:  # Überprüft, ob der Alarm ausgeschaltet wurde
+        for _ in range(10):
+            if not alarm_on[index]:
                 break
             winsound.Beep(freq, 400)
             winsound.Beep(freq + 100, 150)
-        time.sleep(1)  # Pausiert für 1 Sekunde zwischen den Alarmen
+        time.sleep(1)
+
+def blink(tab):
+    current_color = tab.cget("background")  # Holt die aktuelle Hintergrundfarbe des Tabs
+    next_color = "red" if current_color == "white" else "white"
+    tab.config(background=next_color)  # Ändert die Hintergrundfarbe des Tabs
 
 
-'''def alarm_manager():
-    print("Alarm-Manager-Funktion wird gestartet")
-    while True:
-        # Warten Sie auf einen Index in der Warteschlange
-        index = alarm_queue.get()
-        # Starten Sie den Alarm-Thread
-        alarm_thread = threading.Thread(target=alarm, args=(index,))
-        alarm_thread.start()
-        print(f"Alarm-Thread für Wecker {index+1} wurde gestartet")
-        # Markieren Sie den Task als erledigt, damit die Warteschlange weiß, dass sie den nächsten Task starten kann
-        alarm_queue.task_done()'''
+def start_blinking(tab):
+    current_color = tab.cget("background")
+    next_color = "red" if current_color == "white" else "white"
+    for _ in range(10):
+        tab.config(background=next_color)
+        time.sleep(1)
+        next_color = "red" if next_color == "white" else "white"
 
-
-def alarm_manager():
-    while True:
-        print(f"Größe der Alarm-Warteschlange: {alarm_queue.qsize()}")
-        # Warten Sie auf einen Index in der Warteschlange
-        index = alarm_queue.get()
-        # Starten Sie den Alarm-Thread
-        alarm_thread = threading.Thread(target=alarm, args=(index,))
-        alarm_thread.start()
-        print(f"Alarm-Thread für Wecker {index+1} wurde gestartet")
-
+def alarm(index):
+    global alarm_on, wecker_zeit
+    if alarm_on[index]:
+        if alarm_typ[index] == 0:
+            alarm_mit_beep(index)
+        else:
+            alarm_mit_musik(index)
+        threading.Thread(target=start_blinking, args=(tabs[index], index)).start()
+        alarm_queue.put(index)
+        while alarm_threads[index].is_alive():
+            time.sleep(0.1)
+        alarm_on[index] = False
+        alarm_threads[index].join()
 
 def snooze(index):
     global alarm_on
-    alarm_on[index] = False
-    snooze_time_in_seconds = snooze_time * 60
-    time.sleep(snooze_time_in_seconds)
-    if not alarm_on[index]:
-        # Fügen Sie den Index zur Warteschlange hinzu, anstatt den Thread hier zu starten
-        alarm_queue.put(index)
+    if wecker_set[index]:
+        alarm_on[index] = False
+        snooze_time_in_seconds = snooze_time * 60
+        time.sleep(snooze_time_in_seconds)
+        if not alarm_on[index]:
+            alarm_queue.put(index)
 
 
 def stop_alarm(index, stunden_spinboxes, minuten_spinboxes):
     global alarm_on
-    alarm_on[index] = False
-    # Aktiviere die Spinboxen wieder
-    stunden_spinboxes[index].config(state="normal")
-    minuten_spinboxes[index].config(state="normal")
+    if wecker_set[index]:
+        alarm_on[index] = False
+        if alarm_threads[index].is_alive():
+            alarm_threads[index].join()
+        delete_alarm(index, stunden_spinboxes, minuten_spinboxes)  # Löscht den Wecker
+        stunden_spinboxes[index].delete(0, 'end')
+        minuten_spinboxes[index].delete(0, 'end')
+        stunden_spinboxes[index].insert(0, '0')
+        minuten_spinboxes[index].insert(0, '0')
 
 
 def delete_alarm(index, stunden_spinboxes, minuten_spinboxes):
-    global alarm_on, wecker_set
-    alarm_on[index] = False
-    wecker_set[index] = False
-    # Leere die Spinboxen
-    stunden_spinboxes[index].delete(0, 'end')
-    minuten_spinboxes[index].delete(0, 'end')
-    # Aktiviere die Spinboxen wieder
-    stunden_spinboxes[index].config(state="normal")
-    minuten_spinboxes[index].config(state="normal")
+    global alarm_on, wecker_set, alarm_typ
+    if wecker_set[index]:
+        alarm_on[index] = False
+        wecker_set[index] = False
+        stunden_spinboxes[index].delete(0, 'end')
+        minuten_spinboxes[index].delete(0, 'end')
+        stunden_spinboxes[index].insert(0, '0')
+        minuten_spinboxes[index].insert(0, '0')
+        stunden_spinboxes[index].config(state="normal")
+        minuten_spinboxes[index].config(state="normal")
+        button_labels[index].set("Alarm")
+        alarm_typ[index] = 0
 
 
 def wecker(index):
@@ -104,29 +129,81 @@ def wecker(index):
             alarm_queue.put(index)
 
 
-# Teil 2
+def create_spinbox(time_frame, from_, to):
+    spinbox = Spinbox(time_frame, from_=from_, to=to, width=5)
+    return spinbox
+
+
+def create_button(tab, text, command, relx, rely):
+    button = Button(tab, text=text, command=command)
+    button.place(relx=relx, rely=rely, anchor=CENTER)
+    return button
+
+
+def is_valid_time(stunden, minuten):
+    return (stunden.isdigit() and minuten.isdigit() and 0 <= int(stunden) < 24
+            and 0 <= int(minuten) < 60)
+
+
+def waehle_musik(index):
+    musik_dateien[index] = filedialog.askopenfilename(filetypes=[("MP3 Files", "*.mp3")])
+
+
+def alarm_mit_musik(index):
+    global alarm_on
+    alarm_on[index] = True
+    pygame.mixer.init()
+    if os.path.isfile(musik_dateien[index]):
+        pygame.mixer.music.load(musik_dateien[index])
+        while alarm_on[index]:
+            pygame.mixer.music.play()
+            time.sleep(1)
+    else:
+        # Wenn die Musikdatei nicht existiert, verwenden Sie den Standard-Alarmton
+        alarm_mit_beep(index)
+
+
+def wechsel_alarm_typ(index):
+    global alarm_typ
+    alarm_typ[index] = 1 - alarm_typ[index]
+    if alarm_typ[index] == 0:
+        button_labels[index].set("Alarm")
+    else:
+        button_labels[index].set("Musik")
+        waehle_musik(index)
+
+
 def set_wecker_zeit(stunden_spinboxes, minuten_spinboxes, index):
     global alarm_on, wecker_set, wecker_zeit
-
     if not wecker_set[index]:
         stunden = stunden_spinboxes[index].get()
         minuten = minuten_spinboxes[index].get()
-        if (stunden.isdigit() and minuten.isdigit() and 0 <= int(stunden) < 24
-                and 0 <= int(minuten) < 60):
+        if is_valid_time(stunden, minuten):
             wecker_zeit[index] = stunden + ":" + minuten
             wecker_set[index] = True
-            alarm_on[index] = True
-            # Deaktiviere die Spinbox des gerade eingestellten Weckers
             stunden_spinboxes[index].config(state="disabled")
             minuten_spinboxes[index].config(state="disabled")
-            # Starte den Wecker
-            wecker_thread = threading.Thread(target=wecker, args=(index,), daemon=True)
-            wecker_thread.start()
+            alarm_queue.put(index)
         else:
             print("Bitte geben Sie eine gültige Zeit ein.")
     else:
         print("Wecker ist bereits eingestellt.")
 
+
+def alarm_manager():
+    while True:
+        index = alarm_queue.get()
+        while True:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M")
+            if wecker_zeit[index] == current_time:
+                break
+            time.sleep(1)
+        alarm_threads[index] = threading.Thread(target=alarm, args=(index,))
+        alarm_threads[index].daemon = True  # Mark the thread as a daemon
+        alarm_threads[index].start()
+        print(f"Alarm-Thread für Wecker {index+1} wurde gestartet")
+        alarm_queue.task_done()
 
 def open_help_window():
     help_window = Toplevel()
@@ -134,23 +211,62 @@ def open_help_window():
     Label(help_window, text="Hier sind die Anweisungen...").pack()
 
 
-def create_window():
+def create_tab(tab_control, stunden_spinboxes, minuten_spinboxes, index):
+    tab = ttk.Frame(tab_control)
+    tab_control.add(tab, text=f"Wecker {index + 1}")
+    tabs.append(tab)  # Fügen Sie den erstellten Tab zur Liste hinzu
+
+    time_frame = Frame(tab)
+    time_frame.place(relx=0.502, rely=0.1, anchor=CENTER)
+
+    stunden_spinboxes[index] = create_spinbox(time_frame, 0, 23)
+    stunden_spinboxes[index].grid(row=0, column=0)
+
+    minuten_spinboxes[index] = create_spinbox(time_frame, 0, 59)
+    minuten_spinboxes[index].grid(row=0, column=1)
+
+    create_button(tab, "Wecker stellen", partial(
+        set_wecker_zeit, stunden_spinboxes, minuten_spinboxes, index), 0.400, 0.4)
+
+    snooze_button = create_button(tab, "Snooze", partial(snooze, index), 0.605, 0.4)
+    snooze_button.config(bg="blue", fg="white")
+
+    stop_button = create_button(tab, "Stop", partial(
+        stop_alarm, index, stunden_spinboxes, minuten_spinboxes), 0.705, 0.4)
+    stop_button.config(bg="red", fg="white")
+
+    create_button(tab, "Wecker löschen", partial(
+        delete_alarm, index, stunden_spinboxes, minuten_spinboxes), 0.400, 0.7)
+
+    button = Button(tab, textvariable=button_labels[index], command=partial(wechsel_alarm_typ, index), width=10)
+    button.place(relx=0.650, rely=0.7, anchor=CENTER)
+
+
+def create_main_window():
     window = Tk()
     window.title("WECKER")
-    window.geometry("400x250")  # Vergrößert das Fenster
-    window.resizable(False, False)  # Verhindert das Ändern der Fenstergröße
-    window.attributes('-toolwindow', True)  # Blendet den Maximierungsbutton aus
+    window.geometry("400x250")
+    window.resizable(False, False)
+    window.attributes('-toolwindow', True)
+    return window
+
+
+def create_window():
+    window = create_main_window()
+
+    # Erstellen Sie die StringVar-Objekte hier, nachdem das Fenster erstellt wurde
+    global button_labels
+    button_labels = [StringVar(window, value="Alarm") for _ in range(3)]
 
     time_label = StringVar()
     time_label.set('00:00:00')
 
     label = Label(window, textvariable=time_label, font=("Helvetica", 30, "bold"), fg="green")
-    label.grid(row=0, column=1, pady=10)  # Fügt vertikalen Abstand hinzu
+    label.grid(row=0, column=1, pady=10)
 
     uhrzeit_thread = threading.Thread(target=uhrzeit, args=(time_label,))
     uhrzeit_thread.start()
 
-    # Starten Sie den Alarm-Manager in einem eigenen Thread
     alarm_manager_thread = threading.Thread(target=alarm_manager)
     alarm_manager_thread.start()
 
@@ -161,36 +277,13 @@ def create_window():
     minuten_spinboxes = [None, None, None]
 
     for i in range(3):
-        tab = ttk.Frame(tab_control)
-        tab_control.add(tab, text=f"Wecker {i + 1}")
-
-        time_frame = Frame(tab)  # Erstellt das time_frame im tab
-        time_frame.place(relx=0.502, rely=0.1, anchor=CENTER)  # Zentriert das time_frame im tab
-
-        stunden_spinboxes[i] = Spinbox(time_frame, from_=0, to=23, width=5)
-        stunden_spinboxes[i].grid(row=0, column=0)
-
-        minuten_spinboxes[i] = Spinbox(time_frame, from_=0, to=59, width=5)
-        minuten_spinboxes[i].grid(row=0, column=1)
-
-        button = Button(tab, text="Wecker stellen", command=lambda index=i: set_wecker_zeit(
-            stunden_spinboxes, minuten_spinboxes, index))
-        button.place(relx=0.400, rely=0.4, anchor=CENTER)  # Fügt horizontalen und vertikalen Abstand hinzu
-
-        snooze_button = Button(tab, text="Snooze/Stop", command=lambda index=i: snooze(index), bg="blue", fg="white")
-        snooze_button.bind("<Double-Button-1>", lambda event, index=i: stop_alarm(
-            index, stunden_spinboxes, minuten_spinboxes))
-        snooze_button.place(relx=0.605, rely=0.4, anchor=CENTER)  # Fügt horizontalen und vertikalen Abstand hinzu
-
-        delete_button = Button(tab, text="Wecker löschen", command=lambda index=i: delete_alarm(
-            index, stunden_spinboxes, minuten_spinboxes))
-        delete_button.place(relx=0.502, rely=0.7, anchor=CENTER)  # Fügt horizontalen und vertikalen Abstand hinzu
+        create_tab(tab_control, stunden_spinboxes, minuten_spinboxes, i)
 
     help_button = Button(window, text="?", command=open_help_window)
     help_button.place(relx=1, x=-2, y=2, anchor=NE)
 
     window.grid_columnconfigure(1, weight=1)
-    window.grid_rowconfigure(1, weight=1)  # Erlaubt es dem tab_control, sich mit dem Fenster zu vergrößern
+    window.grid_rowconfigure(1, weight=1)
 
     window.mainloop()
 
