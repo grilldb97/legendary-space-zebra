878fba3914353f3a9be1a973338003934ae946fd
grilldb97 <dustin.bellem@web.de>
2023-12-28 16:08:57 +0100

    28.12.2023 - Umstrukturierung Wecker

--- wecker/wecker.py
+++ wecker/wecker.py
@@ -1,9 +1,40 @@
-import ui
+from threading import Thread
+from ui import GUI, MyWindow
+from alarms import Alarm
+from queue import Queue
+
+# Define the AlarmThread class
+class AlarmThread(Thread):
+    def __init__(self, alarm_id):
+        super().__init__()
+        self.alarm_id = alarm_id
+
+    def run(self):
+        while True:
+            alarm = queue.get()
+            if alarm is None:
+                break
+
+            alarm.start_alarm()
+
+# Initialize the alarm objects
+alarm_objects = [Alarm(i, i) for i in range(3)]
+
+# Create a queue for scheduling alarms
+queue = Queue()
+
+# Create a list of threads for alarm handling
+threads = [AlarmThread(i) for i in range(3)]
+
+# Create an instance of MyWindow
+my_window = MyWindow(alarm_objects, queue, threads)
+
+def main():
+    # Create the window
+    my_window.create_window()
+
+    # Start the alarm clock
+    GUI.start(my_window.create_window)
 
 if __name__ == "__main__":
-    # Erstelle eine Instanz der GUI-Klasse
-    gui = ui.GUI()
-
-    # Rufe die start()-Methode auf, um die GUI zu initialisieren und zu starten
-    gui.__init__()
-
+    main()
