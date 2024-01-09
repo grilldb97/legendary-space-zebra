29bb28b082d2b3a7436cf628f181976eb2bf2f4f
grilldb97 <dustin.bellem@web.de>
2024-01-09 08:07:45 +0100

    09.01.2024 - Datenbank: Programm erweitert mit Benutzereingabe, Schreiben in die Datenbank und Datenbankinhalt auslesen

--- datenbank1.py
+++ datenbank1.py
@@ -1,6 +1,11 @@
 import sqlite3
+
 verbindung = sqlite3.connect("datenbank1.db")
 zeiger = verbindung.cursor()
+
+nachname = input("Nachname: ")
+vorname = input("Vorname: ")
+geburtstag = input("Geburtstag: ")
 
 sql_anweisung = """
 CREATE TABLE IF NOT EXISTS personen (
@@ -9,7 +14,17 @@ nachname VARCHAR(20),
 geburtstag DATE
 );"""
 
-zeiger.execute(sql_anweisung)
+zeiger.execute("""
+                INSERT INTO personen 
+                       VALUES (?,?,?)
+               """,
+               (vorname, nachname, geburtstag)
+               )
+zeiger.execute("SELECT * FROM personen")
+inhalt = zeiger.fetchall()
+
+for x in inhalt:
+    print(x, "\n")
 
 verbindung.commit()
 verbindung.close()
