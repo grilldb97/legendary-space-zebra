141318f9773fdbea3ad4cd90c76b5e240366be82
grilldb97 <dustin.bellem@web.de>
2024-01-10 08:18:13 +0100

    10.01.2024 - Datenbank: erweitert mit einer Suche für die Datenbank

--- datenbank1.py
+++ datenbank1.py
@@ -1,11 +1,60 @@
 import sqlite3
+import sys
 
 verbindung = sqlite3.connect("datenbank1.db")
 zeiger = verbindung.cursor()
 
-nachname = input("Nachname: ")
-vorname = input("Vorname: ")
-geburtstag = input("Geburtstag: ")
+match input("Was soll ausgeführt werden (1: Hinzufügen 2: Suchen)? "):
+    case "1":
+        nachname = input("Nachname: ")
+        vorname = input("Vorname: ")
+        geburtstag = input("Geburtstag: ")
+
+        zeiger.execute("""
+                INSERT INTO personen 
+                       VALUES (?,?,?)
+               """,
+                       (vorname, nachname, geburtstag)
+                       )
+        zeiger.execute("SELECT * FROM personen")
+        inhalt = zeiger.fetchall()
+
+    case "2":
+        nachname = input("Nachname: ")
+        vorname = input("Vorname: ")
+        geburtstag = input("Geburtstag: ")
+
+        # Platzhalter bei leerer Eingabe
+        if not nachname:
+            nachname = '%'
+        else:
+            nachname += '%'  # Erlaube Wildcards am Ende
+        if not vorname:
+            vorname = '%'
+        else:
+            vorname += '%'  # Erlaube Wildcards am Ende
+        if not geburtstag:
+            geburtstag = '%'
+        else:
+            geburtstag += '%'  # Erlaube Wildcards am Ende
+
+        zeiger.execute("""
+            SELECT * FROM personen WHERE 
+            nachname LIKE ? AND 
+            vorname LIKE ? AND 
+            geburtstag LIKE ?
+        """, (nachname, vorname, geburtstag))
+
+        inhalt = zeiger.fetchall()
+
+        # Ausgabe der Ergebnisse
+        for x in inhalt:
+            print(x)
+    case _:
+        print("Invalid Input")
+        sys.exit(-1)
+
+
 
 sql_anweisung = """
 CREATE TABLE IF NOT EXISTS personen (
@@ -14,14 +63,7 @@ nachname VARCHAR(20),
 geburtstag DATE
 );"""
 
-zeiger.execute("""
-                INSERT INTO personen 
-                       VALUES (?,?,?)
-               """,
-               (vorname, nachname, geburtstag)
-               )
-zeiger.execute("SELECT * FROM personen")
-inhalt = zeiger.fetchall()
+
 
 for x in inhalt:
     print(x, "\n")
