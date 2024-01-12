6de9d8362c83f0dab98df32a6034d6c29c4ce6fd
grilldb97 <dustin.bellem@web.de>
2024-01-12 17:13:56 +0100

    12.01.2024 - Datenbank: Bearbeiten eines Datensatzes überarbeitet; Programm erweitert mit dem Löschen eines Datensatzes

--- datenbank1.py
+++ datenbank1.py
@@ -7,19 +7,19 @@ print("Datenbank - Geburtstage")
 
 
 def search():
-    nachname_search = input("Nachname: ")
     vorname_search = input("Vorname: ")
+    nachname_search = input("Nachname: ")
     geburtstag_search = input("Geburtstag: ")
 
     # Platzhalter bei leerer Eingabe
-    if not nachname_search:
-        nachname_search = '%'
-    else:
-        nachname_search += '%'  # Erlaube Wildcards am Ende
     if not vorname_search:
         vorname_search = '%'
     else:
         vorname_search += '%'  # Erlaube Wildcards am Ende
+    if not nachname_search:
+        nachname_search = '%'
+    else:
+        nachname_search += '%'  # Erlaube Wildcards am Ende
     if not geburtstag_search:
         geburtstag_search = '%'
     else:
@@ -27,24 +27,28 @@ def search():
 
     zeiger.execute("""
             SELECT * FROM personen WHERE 
-            nachname LIKE ? AND 
             vorname LIKE ? AND 
+            nachname LIKE ? AND 
             geburtstag LIKE ?
-        """, (nachname_search, vorname_search, geburtstag_search))
+        """, (vorname_search, nachname_search, geburtstag_search))
 
     inhalt_search = zeiger.fetchall()
-    for x in inhalt_search:
-        print(x)
+    for i, x in enumerate(inhalt_search):
+        print(f"{i+1}. {x}")
 
-    return nachname_search, vorname_search, geburtstag_search
+    return inhalt_search, vorname_search, nachname_search, geburtstag_search
 
 
-def update_field(field_to_update, new_value, nachname_search, vorname_search, geburtstag_search):
+def update_field(field_to_update, new_value, vorname_search, nachname_search, geburtstag_search):
     zeiger.execute(f"""
         UPDATE personen 
         SET {field_to_update} = ? 
-        WHERE nachname LIKE ? AND vorname LIKE ? AND geburtstag LIKE ?
-        """, (new_value, nachname_search, vorname_search, geburtstag_search))
+        WHERE vorname LIKE ? AND nachname LIKE ? AND geburtstag LIKE ?
+        """, (new_value, vorname_search, nachname_search, geburtstag_search))
+    print("Datensatz wurde aktualisiert.")
+    print(f"ALT: Vorname: {vorname_search}, Nachname: {nachname_search}, Geburtstag: {geburtstag_search}")
+    print(f"UPDATED: Vorname: {new_value}, Nachname: {nachname_search}, Geburtstag: {geburtstag_search}")
+    verbindung.commit()
 
 
 sql_anweisung = """
@@ -54,9 +58,17 @@ nachname VARCHAR(20),
 geburtstag DATE
 );"""
 
+
+def delete(vorname_search, nachname_search, geburtstag_search):
+    zeiger.execute("""
+    DELETE FROM personen
+    WHERE vorname=? AND nachname=? AND geburtstag=?
+    """, (vorname_search, nachname_search, geburtstag_search))
+    print(f"Vorname: {vorname_search}, Nachname: {nachname_search}, Geburtstag: {geburtstag_search} wurde gelöscht")
+
 while True:
-    user_input = input("Was soll ausgeführt werden?\n\n 1: Hinzufügen\n 2: Suchen\n 3: Bearbeiten\n 4: Löschen\n "
-                       "5: Beenden")
+    user_input = input("Was soll ausgeführt werden?\n 1: Hinzufügen\n 2: Suchen\n 3: Bearbeiten\n 4: Löschen\n "
+                       "5: Beenden\n")
 
     match user_input:
         case "1":
@@ -72,10 +84,10 @@ while True:
                            )
             zeiger.execute("""
                 SELECT * FROM personen WHERE 
-                nachname == ? AND 
                 vorname == ? AND 
+                nachname == ? AND 
                 geburtstag == ?
-            """, (nachname, vorname, geburtstag))
+            """, (vorname, nachname, geburtstag))
             inhalt = zeiger.fetchall()
             print(inhalt, "wurde der Datenbank hinzugefügt")
             verbindung.commit()
@@ -84,24 +96,40 @@ while True:
             verbindung.commit()
         case "3":  # Bearbeiten
             print("Suchen Sie die Person, wo Sie die Daten bearbeiten möchten")
-            nachname_search, vorname_search, geburtstag_search = search()
+            inhalt_search, vorname_search, nachname_search, geburtstag_search = search()
 
-            field_to_update = input(
-                "Welches Feld möchtest du aktualisieren? (1: Vorname, 2: Nachname, 3: Geburtstag): ")
+            if len(inhalt_search) > 1:
+                auswahl = input("Bitte wählen Sie die Nummer der Person aus, die Sie bearbeiten möchten: ")
+                auswahl = int(auswahl) - 1  # Korrigiere den Index
+                vorname_search, nachname_search, geburtstag_search = inhalt_search[auswahl]
+
+            while True:
+                field_to_update = input("Welches Feld möchtest du aktualisieren? (1: Vorname, 2: Nachname, 3: Geburtstag): ")
+                if field_to_update in ["1", "2", "3"]:
+                    break
+                else:
+                    print("Ungültige Eingabe. Bitte wähle entweder 1, 2 oder 3.")
             new_value = input("Bitte gib den neuen Wert ein: ")
 
             match field_to_update:
                 case "1":
-                    update_field("vorname", new_value, nachname_search, vorname_search, geburtstag_search)
+                    update_field("vorname", new_value, vorname_search, nachname_search, geburtstag_search)
                 case "2":
-                    update_field("nachname", new_value, nachname_search, vorname_search, geburtstag_search)
+                    update_field("nachname", new_value, vorname_search, nachname_search, geburtstag_search)
                 case "3":
-                    update_field("geburtstag", new_value, nachname_search, vorname_search, geburtstag_search)
+                    update_field("geburtstag", new_value, vorname_search, nachname_search, geburtstag_search)
                 case _:
                     print("Ungültige Eingabe. Bitte wähle entweder 1, 2 oder 3.")
             verbindung.commit()
         case "4":  # Löschen
-            pass
+            print("Suchen Sie die Person, wo Sie die Daten löschen möchten")
+            inhalt_search, vorname_search, nachname_search, geburtstag_search = search()
+
+            if len(inhalt_search) > 1:
+                auswahl = input("Bitte wählen Sie die Nummer der Person aus, die Sie bearbeiten möchten: ")
+                auswahl = int(auswahl) - 1  # Korrigiere den Index
+                vorname_search, nachname_search, geburtstag_search = inhalt_search[auswahl]
+            delete(vorname_search, nachname_search, geburtstag_search)
             verbindung.commit()
         case "5":
             print("Programm beendet.")
