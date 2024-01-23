d0684c9a08f596c63d893d65eba4f07ba99752f1
grilldb97 <dustin.bellem@web.de>
2024-01-23 19:38:37 +0100

    23.01.2024 - kivy: BoxLayout

---
+++ kivy_BoxLayout/kivy_boxlayout.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class BoxLayoutApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    BoxLayoutApp().run()
