95f1d284060f1f3dde70ce20b0f9a9c10c3844fc
grilldb97 <dustin.bellem@web.de>
2024-01-24 23:06:57 +0100

    24.01.2024 - kivy: AnchorLayout

---
+++ kivy_Learning/kivy_AnchorLayout.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+
+class AnchorLayoutApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    AnchorLayoutApp().run()
