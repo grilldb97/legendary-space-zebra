4b414486bcc67a9379afa3e5decd7f80ac84fd40
grilldb97 <dustin.bellem@web.de>
2024-01-25 19:06:44 +0100

    25.01.2024 - kivy: GridLayout

---
+++ kivy_Learning/kivy_gridlayout.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class GridLayoutExample(GridLayout):
+    pass
+
+
+class GridLayoutApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    GridLayoutApp().run()
