17fc3055e90a320cd67bd458c76f54ddfb4d6039
grilldb97 <dustin.bellem@web.de>
2024-01-26 11:08:04 +0100

    26.01.2024 - kivy: StackLayout

---
+++ kivy_Learning/kivy_stacklayout.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.stacklayout import StackLayout
+
+
+class StackLayoutExample(StackLayout):
+    pass
+
+
+class StackLayoutApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    StackLayoutApp().run()
