89d0079920ab7efa3c980da9d260b168a6d37e66
grilldb97 <dustin.bellem@web.de>
2024-02-05 20:44:59 +0100

    05.02.2024 - kivy: ProgressBar

---
+++ kivy_Learning/kivy_progressbar.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class WidgetsExample(GridLayout):
+    pass
+
+
+class ProgressBarApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    ProgressBarApp().run()
