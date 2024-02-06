9cfece883bb9d58cf1f1716604e6d8ac03967f69
grilldb97 <dustin.bellem@web.de>
2024-02-06 07:59:24 +0100

    06.02.2024 - kivy: TextInput

---
+++ kivy_Learning/kivy_textinput.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class WidgetsExample(GridLayout):
+    pass
+
+
+class TextInputApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    TextInputApp().run()
