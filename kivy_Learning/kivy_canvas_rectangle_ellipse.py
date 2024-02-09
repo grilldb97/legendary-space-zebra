6a1604fbe63c759dc53e1b3632d90c926eb5c3ce
grilldb97 <dustin.bellem@web.de>
2024-02-09 15:42:10 +0100

    09.02.2024 - kivy: Canvas - Rectangle + Ellipse

---
+++ kivy_Learning/kivy_canvas_rectangle_ellipse.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CanvasExample(Widget):
+    pass
+
+class CanvasRectangleEllipseApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    CanvasRectangleEllipseApp().run()
