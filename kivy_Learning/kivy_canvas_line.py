3c58b7b38711627eb4f6bb46d9b4f68c4402cf7e
grilldb97 <dustin.bellem@web.de>
2024-02-10 22:06:41 +0100

    10.02.2024 - kivy: Canvas - Line

---
+++ kivy_Learning/kivy_canvas_line.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CanvasExample(Widget):
+    pass
+
+
+class CanvasLineApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    CanvasLineApp().run()
