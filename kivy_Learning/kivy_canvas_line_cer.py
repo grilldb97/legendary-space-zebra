8b74d6c487e4146342b66b3e8dbed7955fc38bd9
grilldb97 <dustin.bellem@web.de>
2024-02-11 12:30:27 +0100

    11.02.2024 - kivy: Canvas - Line: circle - ellipse - rectangle

---
+++ kivy_Learning/kivy_canvas_line_cer.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CanvasExample(Widget):
+    pass
+
+
+class CanvasLineCERApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    CanvasLineCERApp().run()
