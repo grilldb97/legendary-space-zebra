c712c0491015702bebf26f152c7e7feffc023f9d
grilldb97 <dustin.bellem@web.de>
2024-02-14 11:24:53 +0100

    14.02.2024 - kivy: Canvas - RelativeLayout

---
+++ kivy_Learning/kivy_canvas_relative_layout.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class CanvasExample(BoxLayout):
+    pass
+
+
+class CanvasRelativeLayoutApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    CanvasRelativeLayoutApp().run()
