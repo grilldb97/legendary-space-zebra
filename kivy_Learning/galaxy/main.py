62d71658f110fc65b47ececf23c928f8d28b0aec
grilldb97 <dustin.bellem@web.de>
2024-02-17 11:23:29 +0100

    17.02.2024 - kivy: Galaxy Game App - Horizontal lines + Horitonzal lines perspective

--- kivy_Learning/galaxy/main.py
+++ kivy_Learning/galaxy/main.py
@@ -10,13 +10,18 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 10
-    V_LINES_SPACING = .1  # Prozent der Fenster-Breite
+    V_LINES_SPACING = .25  # Prozent der Fenster-Breite
     vertical_lines = []
+
+    H_NB_LINES = 15
+    H_LINES_SPACING = .1  # Prozent der Fenster-Breite
+    horizontal_lines = []
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print("INIT W:" + str(self.width) + "H:" + str(self.height))
         self.init_vertical_lines()
+        self.init_horizontal_lines()
 
     def on_parent(self, widget, parent):
         # print("ON PARENT W:" + str(self.width) + "H:" + str(self.height))
@@ -27,6 +32,7 @@ class MainWidget(Widget):
         # self.perspective_point_x = self.width/2
         # self.perspective_point_y = self.height * 0.75
         self.update_vertical_lines()
+        self.update_horizontal_lines()
 
     def on_perspective_point_x(self, widget, value):
         # print("PX:" + str(value))
@@ -46,7 +52,7 @@ class MainWidget(Widget):
     def update_vertical_lines(self):
         central_line_x = int(self.width / 2)
         spacing = self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES / 2)+0.5
+        offset = -int(self.V_NB_LINES / 2) + 0.5
         for i in range(0, self.V_NB_LINES):
             line_x = int(central_line_x + offset * spacing)
 
@@ -55,23 +61,47 @@ class MainWidget(Widget):
             self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
 
+    def init_horizontal_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line())
+
+    def update_horizontal_lines(self):
+        central_line_x = int(self.width / 2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = int(self.V_NB_LINES / 2) - 0.5
+
+        xmin = central_line_x - offset * spacing
+        xmax = central_line_x + offset * spacing
+        spacing_y = self.H_LINES_SPACING * self.height
+
+        for i in range(0, self.H_NB_LINES):
+            line_y = i * spacing_y
+            x1, y1 = self.transform(xmin, line_y)
+            x2, y2 = self.transform(xmax, line_y)
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
+
     def transform(self, x, y):
         # return self.transform_2D(x, y)
         return self.transform_perspective(x, y)
 
-    def transform_2D(self, x, y):
+    @staticmethod
+    def transform_2D(x, y):
         return int(x), int(y)
 
     def transform_perspective(self, x, y):
-        tr_y = y * self.perspective_point_y / self.height
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        lin_y = y * self.perspective_point_y / self.height
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
 
         diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - tr_y
-        proportion_y = diff_y / self.perspective_point_y
+        diff_y = self.perspective_point_y - lin_y
+        factor_y = diff_y / self.perspective_point_y
+        factor_y = pow(factor_y, 4)
 
-        tr_x = self.perspective_point_y + diff_x * proportion_y
+        tr_x = self.perspective_point_y + diff_x * factor_y
+        tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
         return int(tr_x), int(tr_y)
 
 
