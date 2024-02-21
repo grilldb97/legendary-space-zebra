ba6d4d3e2ee549eebbaf813a0f4e625c459be51f
grilldb97 <dustin.bellem@web.de>
2024-02-21 12:05:01 +0100

    21.02.2024 - kivy: Galaxy Game App - Keyboard

--- kivy_Learning/galaxy/main.py
+++ kivy_Learning/galaxy/main.py
@@ -1,12 +1,15 @@
 from kivy.config import Config
+
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
 from kivy.app import App
+from kivy import platform
+from kivy.core.window import Window
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
 from kivy.properties import NumericProperty, Clock
 from kivy.uix.widget import Widget
-
-Config.set('graphics', 'width', '900')
-Config.set('graphics', 'height', '400')
 
 
 class MainWidget(Widget):
@@ -33,7 +36,23 @@ class MainWidget(Widget):
         # print("INIT W:" + str(self.width) + "H:" + str(self.height))
         self.init_vertical_lines()
         self.init_horizontal_lines()
+
+        if self.is_desktop():
+            self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
+            self._keyboard.bind(on_key_down=self.on_keyboard_down)
+            self._keyboard.bind(on_key_up=self.on_keyboard_up)
+
         Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self.on_keyboard_down)
+        self._keyboard.unbind(on_key_up=self.on_keyboard_up)
+        self._keyboard = None
+
+    def is_desktop(self):
+        if platform in ('linux', 'windows', 'macosx'):
+            return True
+        return False
 
     def on_parent(self, widget, parent):
         # print("ON PARENT W:" + str(self.width) + "H:" + str(self.height))
@@ -99,8 +118,7 @@ class MainWidget(Widget):
         # return self.transform_2D(x, y)
         return self.transform_perspective(x, y)
 
-    @staticmethod
-    def transform_2D(x, y):
+    def transform_2D(self, x, y):
         return int(x), int(y)
 
     def transform_perspective(self, x, y):
@@ -117,6 +135,17 @@ class MainWidget(Widget):
         tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
         return int(tr_x), int(tr_y)
 
+    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'left':
+            self.current_speed_x = self.SPEED_X
+        elif keycode[1] == 'right':
+            self.current_speed_x = -self.SPEED_X
+        return True
+
+    def on_keyboard_up(self, keyboard, keycode):
+        self.current_speed_x = 0
+        return True
+
     def on_touch_down(self, touch):
         if touch.x < self.width / 2:
             print("<-")
@@ -131,7 +160,7 @@ class MainWidget(Widget):
 
     def update(self, dt):
         # print("dt: " + str(dt*60))
-        time_factor = dt*60
+        time_factor = dt * 60
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.current_offset_y += self.SPEED * time_factor
